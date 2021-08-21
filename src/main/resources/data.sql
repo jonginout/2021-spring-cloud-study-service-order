987b6a257bff5a14058b16463d81189708d89591
jongin <jongin@toss.im>
2021-08-21 18:58:34 +0900

    2021-08-21

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
 insert into orders(order_id, product_id, qty, total_price, unit_price, user_id)
-    values('4392b046-6358-48bf-876b-8da2f15710d6', 'catalog-001', 10, 140000, 14000, 'a0f1cb8a-515d-4083-b215-fb5634551069');
+    values('4392b046-6358-48bf-876b-8da2f15710d6', 'catalog-001', 10, 140000, 14000, '6d2d0064-3f35-4049-b778-a16a56f0ab6d');
+
+insert into orders(order_id, product_id, qty, total_price, unit_price, user_id)
+    values('8031986d-1450-4078-b7b0-472b933c5ab2', 'catalog-002', 12, 168144, 14012, '6d2d0064-3f35-4049-b778-a16a56f0ab6d');
+
+insert into orders(order_id, product_id, qty, total_price, unit_price, user_id)
+    values('8d83fcac-06f1-454a-a666-e6de5c850b9b', 'catalog-003', 3, 29700, 9900, '6d2d0064-3f35-4049-b778-a16a56f0ab6d');
